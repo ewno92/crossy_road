1cc68e65ed1482fd23a103425452925105bfd9aa
ewno92 <77653755+ewno92@users.noreply.github.com>
2021-02-10 23:11:01 -0800

    completed

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,26 @@
+from turtle import Turtle
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.penup()
+        self.hideturtle()
+        self.goto(-280, 260)
+        self.color("black")
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(self.level, align="center",
+                   font=FONT)
+
+    def update_level(self):
+        self.level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(-25, 0)
+        self.write(f"GAME OVER")
